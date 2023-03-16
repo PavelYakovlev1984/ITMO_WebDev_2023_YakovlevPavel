6a477bbef1d25903145466875f300c5d2d75d96f
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-03-16 21:46:08 +0300

    Basik HTML sessons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-export default {
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+
+export default defineConfig({
   server: {
     port: 8088,
     host: 'local.dev',
     https: true,
   },
-};
+  plugins: [mkcert()],
+});
