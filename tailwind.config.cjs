79ecc0495cc23cc13a9cfe22d3890cf7a0b428cb
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-03-26 20:34:09 +0300

    add media

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,18 @@ module.exports = {
         'regal-yellow': '#efaf4d',
         'dark-bg': '#00001a',
       },
+    },screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
     },
     plugins: [],
   }
