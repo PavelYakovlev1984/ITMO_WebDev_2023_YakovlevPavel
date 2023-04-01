3d149b218d8f8dc0b1afe6b388ed72bc0d47a3de
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-04-01 13:16:11 +0300

    and lesson

--- main.js
+++ main.js
@@ -1,24 +1,59 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const DOM = document.getElementById.bind(document);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const domInputName = DOM("inpName");
+const domInputSurname = DOM("inpSurname");
+const domConResult = DOM("conResult");
 
-setupCounter(document.querySelector('#counter'))
+let fullName = "";
+
+domInputName.oninput = onInNameInput;
+domInputSurname.oninput = onInSurnameInput;
+
+function renderFullName() {
+  fullName = domInputSurname.value + " " + domInputSurname.value;
+  domConResult.textContext = fullName;
+}
+
+function onInNameInput(e) {
+  console.log("onInNameInput", { e });
+  renderFullName();
+}
+
+function onInSurnameInput(e) {
+  e.stopPropagation();
+  console.log("onInSurnameInput", { e });
+  renderFullName();
+}
+function add(a, b) {
+  const result = a + b;
+  console.log("add:", a, b, result);
+}
+
+console.log(domInputName, domInputSurname, domConResult);
+
+// let name = `Pavel`;
+// console.log(name);
+// let yearName = 234;
+// console.log(yearName);
+//
+// let febonachi = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
+// let arr = new Array(10);
+// let user = {
+//   name: "",
+//   year: "",
+//   yearName: 1987,
+// };
+// arr[10] = febonachi;
+// console.log(arr, febonachi);
+// console.log(user);
+//
+// class User {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+// }
+//
+// let userPavel = new User("Pavel", 1987);
+// let userPavel2 = new User("Pavel", 1987);
+// console.log(userPavel, userPavel2);
