52c14aab5a360d352d9b63e0d1666c01347b91f7
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-04-06 18:39:42 +0300

    add

--- main.js
+++ main.js
@@ -2,25 +2,24 @@ const appendBlock = (block) => document.getElementById("app").appendChild(block)
 
 const createBlock = (x, y, size = 50, color) => {
   const result = document.createElement("div");
-  const isEmpty = Math.random() > 0.5;
-  if (color){
+  const isNotEmpty = Math.random() > 0.5;
+  if (color) {
     result.style.backgroundColor = color;
   } else {
     const isNotEmpty = Math.random() > 0.5;
-    if (isNotEmpty)
+    if (isNotEmpty) {
       result.style.backgroundColor = "black";
+      result.style.width = result.style.height = `${size}px`;
+      result.style.position = "absolute";
+      result.style.left = `${x}px`;
+      result.style.top = `${y}px`;
+      return result;
     }
-
   }
-  result.style.width = result.style.height = `${size}px`;
-  result.style.position = "absolute";
-  result.style.left = `${x}px`;
-  result.style.top = `${y}px`;
-  return result;
 };
-
+//
 const BLOCK_SIZE = 50;
-
+//
 let columns = 5;
 let xPos = 0;
 let yPos = 0;
@@ -28,7 +27,7 @@ let yPos = 0;
 const line = [];
 
 while (columns-- > 0) {
-  const block = createBlock(xPos, yPos, BLOCK_SIZE);
+  const block = createBlock(xPos, yPos, BLOCK_SIZE, "blue");
   line.push(block);
   xPos += BLOCK_SIZE;
   appendBlock(block);
