ce5e6a08cb00040349963fe99d697d18ac1e30bd
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-04-06 20:45:09 +0300

    corect pixels monsters generator

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
-const appendBlock = (block) => document.getElementById("app").appendChild(block);
-
-const createBlock = (x, y, size = 50, color) => {
-  const result = document.createElement("div");
+const DOM = (id) => document.getElementById(id);
+const appendBlockToContainer = (block, container) => container.appendChild(block);
+const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const getColorOrEmptyOnRandom = () => {
   const isNotEmpty = Math.random() > 0.5;
+  if (isNotEmpty) {
+    return "black";
+  }
+  return "red";
+};
+const createBlockWithParamsAtPosition = (x, y, size, color) => {
+  const result = document.createElement("div");
   if (color) {
     result.style.backgroundColor = color;
-  } else {
-    const isNotEmpty = Math.random() > 0.5;
-    if (isNotEmpty) {
-      result.style.backgroundColor = "black";
-      result.style.width = result.style.height = `${size}px`;
-      result.style.position = "absolute";
-      result.style.left = `${x}px`;
-      result.style.top = `${y}px`;
-      return result;
-    }
   }
+  console.log(x, y);
+  result.style.width = result.style.height = `${size}px`;
+  result.style.position = "absolute";
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
+  return result;
 };
-//
 const BLOCK_SIZE = 50;
-//
-let columns = 5;
+const STEP_DELTA_X = BLOCK_SIZE;
+const DIMENSION = 8;
+const DIMENSION_HALF = DIMENSION / 2;
+const container = DOM("app");
+let columns = 0;
+let rows = 0;
 let xPos = 0;
 let yPos = 0;
-
-const line = [];
-
-while (columns-- > 0) {
-  const block = createBlock(xPos, yPos, BLOCK_SIZE, "blue");
-  line.push(block);
-  xPos += BLOCK_SIZE;
-  appendBlock(block);
+// while (rows-- > 0) {
+let colorsInLine = [];
+while (columns < DIMENSION_HALF) {
+  const color = getColorOrEmptyOnRandom();
+  const block = createBlockWithParamsAtPosition(xPos, yPos, BLOCK_SIZE, color);
+  colorsInLine.push(color);
+  appendBlockToContainer(block, container);
+  xPos += STEP_DELTA_X;
+  columns += 1;
 }
-line.reverse().forEach((block) => {
-  xPos += BLOCK_SIZE;
-});
+// const rightHalfOffsetX = (DIMENSION / 2) * BLOCK_SIZE;
+// colorsInLine.reverse().forEach((color, index) => {
+//   const block = createBlock(BLOCK_SIZE * index + rightHalfOffsetX, yPos, BLOCK_SIZE, color);
+//   appendBlockToContainer(block);
+// });
+//   yPos += BLOCK_SIZE;
+// }
