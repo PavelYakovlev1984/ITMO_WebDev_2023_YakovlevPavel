6cc2bf9ced04a500de9e7754e07a91730e24bd02
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-04-08 10:17:37 +0300

    end 13 lessons - show cart

---
+++ main.js
@@ -0,0 +1,17 @@
+import "@unocss/reset/tailwind.css";
+import "uno.css";
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask = document.getElementById("popupCreateTask");
+
+domBtnCreateTask.onclick = () => {
+  console.log("click");
+  domPopupCreateTask.classList.remove("hidden");
+
+  const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
+
+  domBtnCloseCreateTaskPopup.onclick = () => {
+    domPopupCreateTask.classList.add("hidden");
+    domBtnCloseCreateTaskPopup.onclick = null;
+  };
+};
