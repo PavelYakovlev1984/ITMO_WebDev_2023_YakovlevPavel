0180794d0b9e96e24091998b391714078c5e1a2c
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-04-25 21:49:36 +0300

    lesson 20 lessons start right (not errors)

--- main.js
+++ main.js
@@ -27,6 +27,13 @@ domTemplateTask.removeAttribute('id');
 domTemplateTask.remove();
 
 const rawTasks = localStorage.getItem(KEY_LOCAL_TASKS);
+fetch('http://localhost:3000/tasks').then( (response)  => {
+  return response.ok && response.json();
+}).then((json) => {
+  if (json && json instanceof  Object) {
+    console.log('json', json);
+  }
+})
 
 const tasks = rawTasks
   ? JSON.parse(rawTasks).map((json) => TaskVO.fromJSON(json))
@@ -174,11 +181,36 @@ async function renderTaskPopup(
   document.onkeyup = (e) => {
     if (e.key === 'Escape') {
       onClosePopup();
-    }
-  };
+  //   }
+  // };
   domPopupContainer.append(taskPopupInstance.render());
   // }, 1000);
 }
+
+delay(1000).then(() => {
+  console.log('render 1');
+  domSpinner.remove();
+  document.onkeyup = (e) => {
+    if (e.key === 'Escape') {
+      onClosePopup();
+    }
+  };
+  domPopupContainer.append(taskPopupInstance.render());
+});
+
+new Promise((resolve, reject) => {
+  console.log('render2');
+  resolve();
+  console.log("render2-1");
+}).then(() => {
+  console.log('render -> then');
+}).catch(() => {
+  console.log('render -> catch');
+}).finally(()=> {
+  console.log('render -> finally');
+});
+
+console.log('render 0');
 
 function saveTask() {
   localStorage.setItem(KEY_LOCAL_TASKS, JSON.stringify(tasks));
