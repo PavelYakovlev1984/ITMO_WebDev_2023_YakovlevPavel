22c32f3bfa1820121c0ea1890b3903b2cb8fa6ee
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-05-04 19:29:05 +0300

    lesson 24 lessons start  update and planet  corect

--- main.js
+++ main.js
@@ -1,60 +1,16 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Planet {
-  x;
-  y;
-  pX;
-  pY;
-  radius;
-  size;
-  atmosphere;
-
-  speed;
-  constructor(x, y, speed = 0.1, size = 10, atmosphere, radius) {
-    this.pX = x;
-    this.pY = y;
-    this.atmosphere = atmosphere;
-    this.radius = radius;
-    this.size = size;
-    this.speed = speed;
-    this.alpha = 0;
-  }
-  render(ctx) {
-    ctx.beginPath();
-    ctx.fillStyle = this.atmosphere;
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-  }
-
-  move() {
-    this.x = planet.radius * Math.sin(this.alpha) + this.pX;
-    this.y = planet.radius * Math.cos(this.alpha) + this.pY;
-    this.alpha += (SPEED_MULT * Math.PI) / 180;
-  }
-}
-
-let SPEED_MULT = 0.2;
-
-let alpha = 0;
-
-class Position {
-  x;
-  y;
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+const centerPosition = new Position(canvas.width / 2, canvas.height / 2);
 
 const planets = [
-  new Planet(100, 150, 30, 0.1, 'red', 10),
-  new Planet(200, 150, 10, 0.1, 'blue', 50),
-  new Planet(200, 100, 20, 0.1, 'green', 100),
-  new Planet(100, 200, 50, 0.1, 'gold', 150),
+  new Planet(centerPosition, 1.5, 30, 'red', 100),
+  new Planet(centerPosition, 0.5, 40, 'blue', 150),
+  new Planet(centerPosition, 0.2, 50, 'green', 200),
+  new Planet(centerPosition, 0.1, 60, 'gold', 150),
 ];
 
 // move();
---
+++ src/solor-system.js
@@ -0,0 +1,43 @@
+
+class Planet {
+  x;
+  y;
+  position;
+  radius;
+  size;
+  atmosphere;
+
+  speed;
+  constructor(position, speed = 0.1, size = 10, atmosphere, radius) {
+    this.position = position;
+    this.atmosphere = atmosphere;
+    this.radius = radius;
+    this.size = size;
+    this.speed = speed;
+    this.alpha = 0;
+  }
+  render(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.atmosphere;
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  move() {
+    this.x = planet.radius * Math.sin(this.alpha) + this.position.x;
+    this.y = planet.radius * Math.cos(this.alpha) + this.position.y;
+    this.alpha += (this.speed * Math.PI) / 180;
+  }
+}
+
+class Position {
+  x;
+  y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export default 
