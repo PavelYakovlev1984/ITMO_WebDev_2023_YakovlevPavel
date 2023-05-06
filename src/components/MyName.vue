4faac24c8f58fb3021512a32f47182dea504f87f
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-05-06 13:40:30 +0300

    lesson 27 vue init project, vue beggining   next

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Enter you Name</h1>
+  <div
+    class=""
+    style="color: red"
+  >
+    {{ firstName }} - {{ lastName }}
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "MyName",
+  data: () => ({
+    firstName: 'Yakovlev',
+    lastName: 'Pavel'
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
